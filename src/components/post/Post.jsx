7560fd80289aed156350e8afd4f49b6ae50fbc56
sparkie11 <gautamwise.s@gmail.com>
2024-10-26 12:21:46 +0530

    today

---
+++ src/components/post/Post.jsx
@@ -0,0 +1,16 @@
+import './post.css'
+
+export default function Post() {
+  return (
+    <div className="post">
+      <div className="postWrapper">
+        <div className="postTop"></div>
+        <div className="postCenter"></div>
+        <div className="postBottom"></div>
+      </div>
+    </div>
+  )
+}
+
+
+// time stoped 1 00 07  feed 
