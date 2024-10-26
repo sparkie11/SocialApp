3e4cfa5970b69395f9476fde449ba94552ca0c6e
sparkie11 <gautamwise.s@gmail.com>
2024-10-26 11:57:35 +0530

    today

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,6 +1,11 @@
+import Share from "../share/Share"
 import "./feed.css"
 export default function Feed() {
   return (
-    <div className="feed">Feed</div>
+    <div className="feed">
+        <div className="feedWrapper">
+            <Share/>
+        </div>
+    </div>
   )
 }
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -15,6 +15,44 @@ export default function Sidebar() {
                  <span className="sidebarListItemText"> Feed </span> </li>
                  <li className="sidebarListItem"> <RssFeed className="sidebarIcon"/>
                  <span className="sidebarListItemText"> Feed </span> </li>
+                 <li className="sidebarListItem"> <RssFeed className="sidebarIcon"/>
+                 <span className="sidebarListItemText"> Feed </span> </li>
+                 <li className="sidebarListItem"> <RssFeed className="sidebarIcon"/>
+                 <span className="sidebarListItemText"> Feed </span> </li>
+                 <li className="sidebarListItem"> <RssFeed className="sidebarIcon"/>
+                 <span className="sidebarListItemText"> Feed </span> </li>
+                 <li className="sidebarListItem"> <RssFeed className="sidebarIcon"/>
+                 <span className="sidebarListItemText"> Feed </span> </li>
+                 <li className="sidebarListItem"> <RssFeed className="sidebarIcon"/>
+                 <span className="sidebarListItemText"> Feed </span> </li>
+            </ul>
+            <button className="sidebarButton">Show More</button>
+            <hr className="sidebarHr"/>
+            <ul className="sidebarFriendList">
+                <li className="sidebarFriend">
+                    <img src="/Assets/bik1.png" alt="" className="sidebarFriendImg" />
+                    <span className="sidebarFriendName"> Jane Doe</span>
+                </li>
+                <li className="sidebarFriend">
+                    <img src="/Assets/bike2.png" alt="" className="sidebarFriendImg" />
+                    <span className="sidebarFriendName"> Jake Doe</span>
+                </li>                <li className="sidebarFriend">
+                    <img src="/Assets/bike3.png" alt="" className="sidebarFriendImg" />
+                    <span className="sidebarFriendName"> Jane Doe</span>
+                </li>                <li className="sidebarFriend">
+                    <img src="/Assets/bike4.png" alt="" className="sidebarFriendImg" />
+                    <span className="sidebarFriendName"> Jane Doe</span>
+                </li>
+                <li className="sidebarFriend">
+                    <img src="/Assets/bike2.png" alt="" className="sidebarFriendImg" />
+                    <span className="sidebarFriendName"> Jake Doe</span>
+                </li>                <li className="sidebarFriend">
+                    <img src="/Assets/bike3.png" alt="" className="sidebarFriendImg" />
+                    <span className="sidebarFriendName"> Jane Doe</span>
+                </li>                <li className="sidebarFriend">
+                    <img src="/Assets/bike4.png" alt="" className="sidebarFriendImg" />
+                    <span className="sidebarFriendName"> Jane Doe</span>
+                </li>
             </ul>
         </div>
     </div>
